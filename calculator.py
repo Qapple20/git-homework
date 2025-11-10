c36a24a5e0d09888533d864bf904398f20785d80
Quintin Applegate <applegateq@wittenberg.edu>
2025-11-10 14:38:06 -0500

    Added functions and methodology for app and calculator

--- calculator.py
+++ calculator.py
@@ -3,3 +3,11 @@ Author: Quintin Applegate
 Title: Arithmetic Calculator
 
 """
+#library 
+from typing import Union
+#Define a combined data type
+Number = Union[int,float]
+
+#Define add function
+def add(a, b):
+    return a+b
